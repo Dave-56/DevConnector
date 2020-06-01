7db9f94880bae03ec29abc3fdb6da56947ab602a
Dave-56 <pemakenemi56@gmail.com>
2020-06-01 13:28:06 -0400

    new commit

---
+++ .history/client/src/components/dashboard/Experience_20200601131316.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+const Experience = ({ experience }) => {
+    const experiences = experience.map(exp => (
+        <td key={exp.id}>
+            <td>{exp.company</td>
+                <td>{exp.company</td>
+        </td>
+    ));
+  return;
+  <Fragment>
+    <h2 className='my-2'>Experience credentials</h2>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th className='hide-sm'>Title</th>
+          <th className='hide-sm'>Years</th>
+        </tr>
+      </thead>
+      <tbody>{experiences}</tbody>
+    </table>
+  </Fragment>;
+};
+
+Experience.propTypes = {};
+
+export default Experience;
