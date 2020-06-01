bad947dc636dface9d9847b0761ebfdedbdf4363
Dave-56 <pemakenemi56@gmail.com>
2020-06-01 15:29:05 -0400

    New changes

---
+++ .history/client/src/actions/profile_20200601152416.js
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+
+//Get current user's profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await api.get('/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Create or update a profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    //Redirecting in action (redux)
+    if (!edit) {
+      history.push('/dashboard');
+    }
+
+    dispatch(
+      setAlert(edit ? 'Profile Updated' : 'Profiile Created'),
+      'success'
+    );
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add experience
+export const addExperience = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Experience added', 'success'));
+    history.push('/dashboard'); //Redirecting in action (redux)
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add education
+export const addEducation = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/profile/education', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education added', 'success'));
+    history.push('/dashboard'); //Redirecting in action (redux)
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
---
+++ .history/client/src/components/routing/Routes_20200601134733.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import ProfileForm from '../profile-forms/ProfileForm';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
+
+import PrivateRoute from '../routing/PrivateRoute';
+
+const Routes = (props) => {
+  return (
+    <section className='container'>
+      <Alert />
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+        <PrivateRoute exact path='/edit-profile' component={ProfileForm} />
+        <PrivateRoute exact path='/add-experience' component={AddExperience} />
+        <PrivateRoute exact path='/add-education' component={AddEducation} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes;
